5b2bb7e956efa31e11276e56f6fafaa2c4e208fd
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-02-11 11:03:59 +0600

    add new apa

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
+// JavaScript if-else condition
+const mytext = document.getElementById("mytext");
+const mybtn = document.getElementById("mybtn");
+const result = document.getElementById("result");
+let age;
+
+mybtn.onclick = function() {
+    age = mytext.value;
+    age = Number(age);
+    if (age >= 18) {
+        result.textContent = "You are old";
+    } else {
+        result.textContent = "You are young";
+    }
+};
+
+
 
 
